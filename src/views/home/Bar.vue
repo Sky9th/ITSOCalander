6d0cb07850ea7ef973031689cc7b34d4cbe8f94c
Sky9th <weitianxu@qq.com>
2021-11-28 16:55:49 +0800

    [+] frame init

---
+++ src/views/home/Bar.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-card classs="top-bar">
+        <div class="top-bar-content">
+            <el-button-group>
+                <el-button size="mini" type="primary" icon="el-icon-plus"></el-button>
+            </el-button-group>
+            <el-button-group>
+                <el-button size="mini" type="default">登陆</el-button>
+                <el-button size="mini" type="success">注册</el-button>
+            </el-button-group>
+        </div>
+    </el-card>
+</template>
+
+<script>
+    export default {
+        name: "Bar"
+    }
+</script>
+
+<style scoped>
+    .top-bar-content { display: flex; justify-content: space-between; width: 100%; }
+</style>
