e794724f725450299832a09f7705562abf0eb1b2
Sky9th <weitianxu@qq.com>
2021-11-28 11:42:29 +0800

    [+] init project

---
+++ src/views/calendar/calendar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>calendar</h1>
+        <FullCalendar :options="calendarOptions" />
+    </div>
+</template>
+
+<script>
+    import '@fullcalendar/core/vdom' // solves problem with Vite
+    import FullCalendar from '@fullcalendar/vue'
+    import dayGridPlugin from '@fullcalendar/daygrid'
+    import interactionPlugin from '@fullcalendar/interaction'
+
+    export default {
+        components: {
+            FullCalendar // make the <FullCalendar> tag available
+        },
+        data() {
+            return {
+                calendarOptions: {
+                    plugins: [ dayGridPlugin, interactionPlugin ],
+                    initialView: 'dayGridMonth'
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
